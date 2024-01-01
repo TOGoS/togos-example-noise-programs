2c30ffb3c2a3bea3ed4c8056d893922dc09d9c12
TOGoS <togos00@gmail.com>
2024-01-01 14:11:25 -0600

    test.lua tests

--- conv.lua
+++ conv.lua
@@ -32,8 +32,17 @@ function is_list(tab)
 	return true
 end
 
-function to_json(thing)
+function to_json(thing, indent_delta, lsep, indent)
 	local typ = type(thing)
+	if indent_delta == nil then indent_delta = "" end
+	if lsep == nil then
+		if indent_delta == "" then
+			lsep = " "
+		else
+			lsep = "\n"
+		end
+	end
+	if indent == nil then indent = "" end
 	
 	if typ == "nil" then
 		return "null"
@@ -42,21 +51,27 @@ function to_json(thing)
 	elseif typ == "number" or typ == "boolean" then
 		return tostring(thing)
 	elseif typ == "table" then
+		local subindent = indent .. indent_delta
+		local is_non_empty = false
 		if is_list(thing) then
 			local json = "["
-			local sep = ""
+			local sep = lsep .. subindent
 			for k, v in pairs(thing) do
-				json = json .. sep .. to_json(v)
-				sep = ", "
+				json = json .. sep .. to_json(v, indent_delta, lsep, subindent)
+				sep = "," .. lsep .. subindent
+				is_non_empty = true
 			end
+			if is_non_empty then json = json .. lsep .. indent end
 			return json .. "]"
 		else
 			local json = "{"
-			local sep = ""
+			local sep = lsep .. subindent
 			for k, v in pairs(thing) do
-				json = json .. sep .. to_json(tostring(k)) .. ": " .. to_json(v)
-				sep = ", "
+				json = json .. sep .. to_json(tostring(k)) .. ": " .. to_json(v, indent_delta, lsep, subindent)
+				sep = "," .. lsep .. subindent
+				is_non_empty = true
 			end
+			if is_non_empty then json = json .. lsep .. indent end
 			return json .. "}"
 		end
 	else
--- test.lua
+++ test.lua
@@ -20,8 +20,20 @@ local function extract_named_noise_expressions(data)
 	return nnes
 end
 
+local function assert_equals(expected, actual)
+ 	if expected ~= actual then
+		error("Assertion failure; expected " .. expected .. " but got " .. actual)
+	end
+end
+
 local conv = require "conv"
 local noise = require "noise"
 local tne = noise.to_noise_expression
 
-print(conv.to_json(extract_named_noise_expressions(data)))
+assert_equals('"foo"', conv.to_json("foo"))
+assert_equals('123', conv.to_json(123))
+assert_equals('[]', conv.to_json{})
+assert_equals('{ "foo": "bar", "baz": "quux" }', conv.to_json{ foo = "bar", baz = "quux" })
+assert_equals('{\n\t"foo": "bar",\n\t"baz": "quux"\n}', conv.to_json({ foo = "bar", baz = "quux" }, "\t"))
+
+-- print(conv.to_json(extract_named_noise_expressions(data), "\t"))
